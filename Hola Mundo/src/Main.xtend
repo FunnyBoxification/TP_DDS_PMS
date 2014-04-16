cb430f297f4f09e88a1e62d917afd33f3be50469
FunnyBoxification <pablorom93@gmail.com>
2014-04-16 11:21:45 -0300

    Agregue otro objeto en el main para probar pushear desde eclipse

--- Hola Mundo/src/Main.xtend
+++ Hola Mundo/src/Main.xtend
@@ -2,5 +2,6 @@
 class Main {
 	def static void main(String[] args) {
 		new Saludador("Aloha").saludar()
+		new Saludador("Hello").saludar()
 	}
 }
--- Hola Mundo/xtend-gen/Main.java
+++ Hola Mundo/xtend-gen/Main.java
@@ -3,5 +3,7 @@ public class Main {
   public static void main(final String[] args) {
     Saludador _saludador = new Saludador("Aloha");
     _saludador.saludar();
+    Saludador _saludador_1 = new Saludador("Hello");
+    _saludador_1.saludar();
   }
 }
